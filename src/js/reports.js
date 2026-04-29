42c616b839142f52a9eee2f21664f05d2f800e19
MohmmedAtef <mohammedateftawfic@gmail.com>
2026-04-29 15:01:50 +0300

    Finish  reports page

--- src/js/reports.js
+++ src/js/reports.js
@@ -1,7 +1,9 @@
-const ctx = document.getElementById('incomeExpenseChart').getContext('2d');
+const ctx1 = document.getElementById('incomeExpenseChart').getContext('2d');
+const ctx2 = document.getElementById('categorySpendingChart').getContext('2d');
 
-const myChart = new Chart(ctx, {
+const incomeExpenseChart = new Chart(ctx1, {
     type: 'line', 
+    
     data: {
         labels: ['Jul 25', 'Aug 25', 'Sep 25', 'Oct 25', 'Nov 25', 'Dec 25'],
         datasets: [
@@ -38,10 +40,56 @@ const myChart = new Chart(ctx, {
                 position: 'bottom', 
             }
         },
+        
         scales: {
             y: {
                 beginAtZero: true 
             }
         }
     }
-});
+} );
+
+const categorySpendingChart = new Chart( ctx2, {
+    type: 'bar',
+    data:{
+        labels: ['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'],
+        datasets:[
+            {
+                label: 'Housing',
+                data: [1200, 1200, 1200, 1200, 1200, 1200],
+                backgroundColor: '#3b82f6',
+            },
+            {
+                label: 'Food',
+                data: [650, 720, 580, 670, 630, 680],
+                backgroundColor: '#F59E0B',
+            },
+            {
+                label: 'Transportation',
+                data: [420, 480, 390, 460, 440,450],
+                backgroundColor: '#10B981',
+            },
+            {
+                label: 'Entertainment',
+                data: [280, 350, 230, 310, 280, 320],
+                backgroundColor: '#8B5CF6',
+            },
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom', 
+            }
+        },
+        scales: {
+                x: {
+                    stacked: true,
+                },
+                y: {
+                    stacked: true
+                }
+            }
+    }
+} );
