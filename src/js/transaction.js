32926c9b40b0955df89581368f9800a361658cb7
Adham-Mohamed-Megahed <adhammohamedmegahed@gmail.com>
2026-04-30 12:06:42 +0300

    enhance pop up in transaction page

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -2,9 +2,13 @@ const openBtn = document.getElementById("openModal");
 const modal = document.getElementById("transactionModal");
 const closeBtn = document.getElementById("closeModal");
 
+const incomeBtn = document.getElementById("incomeBtn");
+const expenseBtn = document.getElementById("expenseBtn");
+
 // Open modal
 openBtn.addEventListener("click", () => {
     modal.style.display = "flex";
+    loadCategories("income");
 });
 
 // Close modal
@@ -12,9 +16,32 @@ closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Close when clicking outside
-window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        modal.style.display = "none";
+// Categories
+const incomeCategories = ["Salary", "Bonus", "Investment"];
+const expenseCategories = ["Food", "Transport", "Shopping", "Housing"];
+
+const categorySelect = document.getElementById("categorySelect");
+
+function loadCategories(type) {
+    categorySelect.innerHTML = "";
+
+    let list = type === "income" ? incomeCategories : expenseCategories;
+
+    list.forEach(cat => {
+        let option = document.createElement("option");
+        option.textContent = cat;
+        categorySelect.appendChild(option);
+    });
+
+    incomeBtn.classList.remove("active-type");
+    expenseBtn.classList.remove("active-type");
+
+    if (type === "income") {
+        incomeBtn.classList.add("active-type");
+    } else {
+        expenseBtn.classList.add("active-type");
     }
-});
+}
+
+incomeBtn.onclick = () => loadCategories("income");
+expenseBtn.onclick = () => loadCategories("expense");
