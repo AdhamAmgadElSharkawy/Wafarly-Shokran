56d2d9c2d1f3f721bd4504b2ff14b86a2b8c7844
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2026-05-06 02:35:40 +0300

    create authentication

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from .models import User
 # Create your views here.
+def signup(request):
+    if request.method =='POST':
+
+        firstName = request.POST.get('first_name')
+        lastName = request.POST.get('last_name')
+        birthDate = request.POST.get('birth_date')
+        userName = request.POST.get('username')
+        Email = request.POST.get('email')
+        passWord = request.POST.get('password')
+        confirmPassWord = request.POST.get('confirm_password')
+        phoneNumber = request.POST.get('phone_number')
+        if passWord != confirmPassWord:
+            return render(request,'signup.html',{'error':'password do not match'}) 
+        try:
+            user = User.objects.create_user(
+                username=userName,
+                email=Email,
+                password=passWord,
+                birth_date= birthDate,
+                phone_number = phoneNumber,
+                first_name = firstName,
+                last_name = lastName
+            )
+            login(request,user)
+            return redirect('dashboard')
+        except Exception as e:
+            print("======== THE REAL ERROR ========")
+            print(e)
+            print("================================")
+            return render(request, 'signup.html', {'error': 'Username already exists or invalid data!'})
+    
+    return render(request,'signup.html')
+
+def login_view(request):
+    if request.method =='POST':
+        Email = request.POST.get('email')
+        passWord = request.POST.get('password')
+
+        user_obj = User.objects.filter(email =Email).first() 
+
+        if user_obj:
+
+            user = authenticate(request,username= user_obj.username,password = passWord)
+            if user is not None:
+                login(request,user)
+                return redirect('dashboard')
+            
+        return render(request,'login.html',{'error':'Invalid email or password'})
+    return render(request,'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'dashboard.html')
