3e3dc02f739b499df08874e43c0690d6549b4ef6
MohmmedAtef <mohammedateftawfic@gmail.com>
2026-05-07 15:06:33 +0300

    group data by date

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,54 +1,51 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from transactions.models import Transaction
-from django.db.models.functions import TruncMonth
-from django.db.models import Sum,Q
+from django.db.models.functions import TruncMonth,TruncDay,TruncDate
+from django.db.models import Sum,Q,Count
 
 # Create your views here.
 @login_required(login_url='login')
 def reports(request):
-    transactionsData=Transaction.objects.filter(user=request.user)
 
-    total_income = 0
-    total_expense = 0
-    
-    for i in transactionsData.filter(type='i'):
-        total_income+=i.amount
-    for e in transactionsData.filter(type='e'):
-        total_expense+=e.amount
-    
-    monthlyData = Transaction.objects.filter(user=request.user).annotate(
+    transactions_data = Transaction.objects.filter(user=request.user).annotate(
         month=TruncMonth('date_time')
-    ).values('month').annotate(
+    ).aggregate(
         total_income=Sum('amount', filter=Q(type='i')),
         total_expense=Sum('amount', filter=Q(type='e')),
-    ).order_by('month')
+        months_count=Count('month', distinct=True) 
+    )
+
+    total_income = float(transactions_data['total_income'] or 0)
+    total_expense = float(transactions_data['total_expense'] or 0)
+    months_count = transactions_data['months_count'] or 0
+
+    if months_count > 0:
+        monthly_avg = (total_income - total_expense) / months_count
+    else:
+        monthly_avg = 0
+
+    daily_data = Transaction.objects.filter(user=request.user).annotate(
+        date=TruncDate('date_time')
+    ).values('date').annotate(
+        total_income=Sum('amount', filter=Q(type='i')),
+        total_expense=Sum('amount', filter=Q(type='e')),
+    ).order_by('date')
 
     transactionsPerCategory= Transaction.objects.filter(type='e',user=request.user).annotate(
-    month=TruncMonth('date_time')
-).values('category__name', 'month').annotate(
+    date=TruncDate('date_time')
+).values('category__name', 'date').annotate(
     total_amount=Sum('amount')
-).order_by('month')
+).order_by('date')
 
-    monthlyAvg=0
-    n=0
-    for d in monthlyData:
-        income = d['total_income'] or 0
-        expense = d['total_expense'] or 0
-        monthlyAvg+= income - expense
-        n+=1
-    
-    if n > 0:
-        monthlyAvg = monthlyAvg / n
-    else:
-        monthlyAvg = 0
 
     context = {
         'ti':total_income,
         'te':total_expense,
         'ts':total_income-total_expense,
-        'monthlyAvg':monthlyAvg,
-        'monthlyData':list(monthlyData),
+        'dailyData':list(daily_data),
+        'monthlyAvg':monthly_avg,
         'transPerCategory':list(transactionsPerCategory),
     }
+
     return render(request,'reports.html',context)
